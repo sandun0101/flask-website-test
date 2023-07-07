f7fcc902421b312a5030457d802a804fbf89fa99
sandun0101 <95874792+sandun0101@users.noreply.github.com>
2023-07-07 07:33:08 +0000

    add html and css
    add css and html. website kind a okay now

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'India',
+  'salary': 'Rs. 150000.00'
+ },
+ {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'US',
+  'salary': '$ 150000.00'
+ },
+  {
+  'id': 3,
+  'title': 'Data Engineer',
+  'location': 'Remote',
+  }
+]
+
 @app.route('/')
 def hello_world():
-  return 'hello world'
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
